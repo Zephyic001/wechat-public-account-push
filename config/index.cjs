e9091188ad3ccac11c1a5e23f27e506c47ead65c
Zephyic001 <1013103268@qq.com>
2024-05-27 02:47:28 +0100

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o0Kjx6vQC9VugtNrUJ_FyNnVkXqQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '0001',
+      useTemplateId: 'IBLk-wfQMOJukGK9Tn26bvcPt73DRQfBVFsrww9XoXI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-04',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '0002',
+  CALLBACK_TEMPLATE_ID: 'ZozMU4AecR0upXVb9eiwcH8fcHreiU0n9IecjtvWWDI',
 
   CALLBACK_USERS: [
     {
